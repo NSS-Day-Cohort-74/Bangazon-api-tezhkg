ec1c084ff4b2f2aba658ce94823580fd442c5e3d
Sabra Kozlina <sjkozlina@gmail.com>
2025-04-01 13:36:31 -0600

    fixed paymenttype customer_id bug

--- bangazonapi/views/paymenttype.py
+++ bangazonapi/views/paymenttype.py
@@ -81,7 +81,9 @@ class Payments(ViewSet):
         """Handle GET requests to payment type resource"""
         payment_types = Payment.objects.all()
 
-        customer_id = self.request.query_params.get('customer', None)
+        customer = Customer.objects.get(user=request.auth.user)
+        customer_id = customer.id
+        
 
         if customer_id is not None:
             payment_types = payment_types.filter(customer__id=customer_id)
