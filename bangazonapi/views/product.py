2d3b5c8a6c8d02507830bb5eb4fd08aa2c9c2703
Nate Z <ntzeitlin@gmail.com>
2025-04-04 11:28:53 -0500

    add incoming data validation using the serializer. Price now has a custom error message. product_category now has a try except block for selecting a category.

--- bangazonapi/views/product.py
+++ bangazonapi/views/product.py
@@ -1,4 +1,5 @@
 """View module for handling requests about products"""
+
 from rest_framework.decorators import action
 from bangazonapi.models.recommendation import Recommendation
 import base64
@@ -15,13 +16,32 @@ from rest_framework.parsers import MultiPartParser, FormParser
 
 class ProductSerializer(serializers.ModelSerializer):
     """JSON serializer for products"""
+
+    price = serializers.DecimalField(
+        max_digits=7,
+        decimal_places=2,
+        error_messages={
+            "invalid": "Please provide a valid price number between 0 and $17,500.",
+        },
+    )
+
     class Meta:
         model = Product
-        fields = ('id', 'name', 'price', 'number_sold', 'description',
-                  'quantity', 'created_date', 'location', 'image_path',
-                  'average_rating', 'can_be_rated', )
+        fields = (
+            "id",
+            "name",
+            "price",
+            "number_sold",
+            "description",
+            "quantity",
+            "created_date",
+            "location",
+            "image_path",
+            "average_rating",
+            "can_be_rated",
+        )
         depth = 1
-    
+
     def validate_price(self, value):
         if value > 17500:
             raise serializers.ValidationError("Price cannot exceed $17,500")
@@ -30,6 +50,7 @@ class ProductSerializer(serializers.ModelSerializer):
 
 class Products(ViewSet):
     """Request handlers for Products in the Bangazon Platform"""
+
     permission_classes = (IsAuthenticatedOrReadOnly,)
 
     def create(self, request):
@@ -92,11 +113,11 @@ class Products(ViewSet):
         data = request.data.copy()
         data["image_path"] = None
 
-        serializer = ProductSerializer(data=data, context={'request':request})
+        serializer = ProductSerializer(data=data, context={"request": request})
 
         if not serializer.is_valid():
-            return Response({"Error": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
-        
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
         new_product = Product()
         new_product.name = request.data["name"]
         new_product.price = request.data["price"]
@@ -105,22 +126,34 @@ class Products(ViewSet):
         new_product.location = request.data["location"]
 
         customer = Customer.objects.get(user=request.auth.user)
+
         new_product.customer = customer
 
-        product_category = ProductCategory.objects.get(pk=request.data["category_id"])
+        try:
+            product_category = ProductCategory.objects.get(
+                pk=request.data["category_id"]
+            )
+        except ProductCategory.DoesNotExist:
+            return Response(
+                {"category_id": "Please select a category"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
         new_product.category = product_category
 
         if "image_path" in request.data:
-            format, imgstr = request.data["image_path"].split(';base64,')
-            ext = format.split('/')[-1]
-            data = ContentFile(base64.b64decode(imgstr), name=f'{new_product.id}-{request.data["name"]}.{ext}')
+            format, imgstr = request.data["image_path"].split(";base64,")
+            ext = format.split("/")[-1]
+            data = ContentFile(
+                base64.b64decode(imgstr),
+                name=f'{new_product.id}-{request.data["name"]}.{ext}',
+            )
 
             new_product.image_path = data
 
         new_product.save()
 
-        result_serializer = ProductSerializer(
-            new_product, context={'request': request})
+        result_serializer = ProductSerializer(new_product, context={"request": request})
 
         return Response(result_serializer.data, status=status.HTTP_201_CREATED)
 
@@ -165,7 +198,7 @@ class Products(ViewSet):
         """
         try:
             product = Product.objects.get(pk=pk)
-            serializer = ProductSerializer(product, context={'request': request})
+            serializer = ProductSerializer(product, context={"request": request})
             return Response(serializer.data)
         except Exception as ex:
             return HttpResponseServerError(ex)
@@ -222,10 +255,12 @@ class Products(ViewSet):
             return Response({}, status=status.HTTP_204_NO_CONTENT)
 
         except Product.DoesNotExist as ex:
-            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+            return Response({"message": ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
 
         except Exception as ex:
-            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            return Response(
+                {"message": ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
 
     def list(self, request):
         """
@@ -258,19 +293,19 @@ class Products(ViewSet):
         products = Product.objects.all()
 
         # Support filtering by category and/or quantity
-        category = self.request.query_params.get('category', None)
-        quantity = self.request.query_params.get('quantity', None)
-        order = self.request.query_params.get('order_by', None)
-        direction = self.request.query_params.get('direction', None)
-        number_sold = self.request.query_params.get('number_sold', None)
-        min_price = self.request.query_params.get('min_price', None)
+        category = self.request.query_params.get("category", None)
+        quantity = self.request.query_params.get("quantity", None)
+        order = self.request.query_params.get("order_by", None)
+        direction = self.request.query_params.get("direction", None)
+        number_sold = self.request.query_params.get("number_sold", None)
+        min_price = self.request.query_params.get("min_price", None)
 
         if order is not None:
             order_filter = order
 
             if direction is not None:
                 if direction == "desc":
-                    order_filter = f'-{order}'
+                    order_filter = f"-{order}"
 
             products = products.order_by(order_filter)
 
@@ -278,17 +313,19 @@ class Products(ViewSet):
             products = products.filter(category__id=category)
 
         if quantity is not None:
-            products = products.order_by("-created_date")[:int(quantity)]
-            
+            products = products.order_by("-created_date")[: int(quantity)]
+
         if min_price is not None:
+
             def price_filter(product):
                 if product.price >= int(min_price):
                     return True
                 return False
-            
+
             products = filter(price_filter, products)
-            
+
         if number_sold is not None:
+
             def sold_filter(product):
                 if product.number_sold >= int(number_sold):
                     return True
@@ -297,10 +334,11 @@ class Products(ViewSet):
             products = filter(sold_filter, products)
 
         serializer = ProductSerializer(
-            products, many=True, context={'request': request})
+            products, many=True, context={"request": request}
+        )
         return Response(serializer.data)
 
-    @action(methods=['post'], detail=True)
+    @action(methods=["post"], detail=True)
     def recommend(self, request, pk=None):
         """Recommend products to other users"""
 
