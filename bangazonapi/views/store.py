602b71489fbf7f8026ac5f1498bf99242d6d7397
Nate Z <ntzeitlin@gmail.com>
2025-04-09 11:47:15 -0500

    StoreSerializer has an additional method field that returns a list of sold products by filtering OrderProducts to products that have been in a completed order and owned by the store's owner, then the individual product data is extracted from the filtered QuerySet and passed through the ProductSerializer.

--- bangazonapi/views/store.py
+++ bangazonapi/views/store.py
@@ -5,13 +5,15 @@ from rest_framework import serializers
 from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from bangazonapi.models import Customer, Product, Store
+from bangazonapi.models import Customer, Product, Store, OrderProduct
 from .product import ProductSerializer
+
 
 class StoreSerializer(serializers.ModelSerializer):
     store_products = serializers.SerializerMethodField()
     size = serializers.SerializerMethodField()
-    
+    sold_products = serializers.SerializerMethodField()
+
     class Meta:
         model = Store
         fields = (
@@ -20,17 +22,32 @@ class StoreSerializer(serializers.ModelSerializer):
             "description",
             "owner",
             "size",
-            "store_products"
+            "store_products",
+            "sold_products",
         )
         depth = 1
 
     def get_store_products(self, obj):
         products = obj.owner.products.all()
         return ProductSerializer(products, many=True).data
-    
+
+    def get_sold_products(self, obj):
+        owner_id = obj.owner.id
+
+        all_products = OrderProduct.objects.filter(
+            product__customer_id=owner_id, order__payment_type__isnull=False
+        )
+        sold_products = []
+
+        for product in all_products:
+            sold_products.append(product.product)
+
+        return ProductSerializer(sold_products, many=True).data
+
     def get_size(self, obj):
         count_of_products = obj.owner.products.count()
         return count_of_products
+
 
 class Stores(ViewSet):
 
@@ -40,42 +57,45 @@ class Stores(ViewSet):
         current_user = Customer.objects.get(user=request.auth.user)
 
         if Store.objects.filter(owner=current_user).exists():
-             return Response({"message":"store already exists for this owner."}, status=status.HTTP_409_CONFLICT)
+            return Response(
+                {"message": "store already exists for this owner."},
+                status=status.HTTP_409_CONFLICT,
+            )
 
         new_store = Store()
         new_store.name = request.data["name"]
         new_store.description = request.data["description"]
         # owner is coming in as the request.auth.user
-        #get user 
+        # get user
         user = request.auth.user
-        #set customer to user 
+        # set customer to user
         customer = Customer.objects.get(user=user)
-        #set owner of the store to customer
+        # set owner of the store to customer
         new_store.owner = customer
-        #set the customer 
-        
+        # set the customer
+
         new_store.save()
-    
-        serializer = StoreSerializer(new_store, context={'request': request})
-    
+
+        serializer = StoreSerializer(new_store, context={"request": request})
+
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-    def list(self,request):
-        
+    def list(self, request):
+
         stores = Store.objects.all()
-        serializer = StoreSerializer(
-            stores, many=True, context={'request':request}
-        )
+        serializer = StoreSerializer(stores, many=True, context={"request": request})
         return Response(serializer.data)
 
     def retrieve(self, request, pk=None):
 
         try:
             store = Store.objects.get(pk=pk)
-            serializer = StoreSerializer(store, many=False, context={"request":request})
+            serializer = StoreSerializer(
+                store, many=False, context={"request": request}
+            )
             return Response(serializer.data)
         except Store.DoesNotExist as ex:
-            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+            return Response({"message": ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
 
     def update(self, request, pk=None):
         store = Store.objects.get(pk=pk)
@@ -88,5 +108,3 @@ class Stores(ViewSet):
         store.save()
 
         return Response({}, status=status.HTTP_204_NO_CONTENT)
-
- 
