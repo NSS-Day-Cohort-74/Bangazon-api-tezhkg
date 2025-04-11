201a18b4caf08661d6295412c04afb6af271c816
Sabra Kozlina <sjkozlina@gmail.com>
2025-04-11 09:06:30 -0600

    added review field to productrating fixtures and model and the productrating post function in products view

--- bangazonapi/models/productrating.py
+++ bangazonapi/models/productrating.py
@@ -8,6 +8,7 @@ class ProductRating(models.Model):
     product = models.ForeignKey("Product", on_delete=models.CASCADE, related_name="ratings")
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     rating = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+    review = models.CharField(max_length=50)
 
 class Meta:
     verbose_name = ("productrating")
