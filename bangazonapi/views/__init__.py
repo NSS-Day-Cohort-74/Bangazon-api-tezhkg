f47e470c029e9e50a6f55162147b111e92b932a0
Edita Adomaityte <adomaityteedita02@gmail.com>
2025-04-17 15:31:32 -0500

    refactoring completed

--- bangazonapi/views/__init__.py
+++ bangazonapi/views/__init__.py
@@ -3,12 +3,10 @@ from .register import login_user
 from .order import Orders
 from .paymenttype import Payments
 from .product import Products
-from .cart import Cart
 from .profile import Profile
 from .productcategory import ProductCategories
 from .lineitem import LineItems
 from .customer import Customers
 from .user import Users
-from .productlike import ProductLike
 from .store import Stores 
 from .reports import Reports
